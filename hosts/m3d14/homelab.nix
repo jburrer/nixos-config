9e46de42a99ba9be3f7606ef437c6dd538c54ce3
n3mo <n3mo@no.one>
2024-07-27 13:12:09 -0400

    first attempt at adding transmission + wireguard

--- hosts/m3d14/homelab.nix
+++ hosts/m3d14/homelab.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   # radicale
   services.radicale = {
@@ -24,40 +24,78 @@
   services.jellyfin = {
     enable = true;
     dataDir = "/srv/state/jellyfin";
-    user = "jellyfin";
-    group = "media";
   };
-  users.users."jellyfin" = {
-    isSystemUser = true;
-    uid = 10010;
-    group = "media";
-  };
+  users.users."jellyfin".extraGroups = [ "media" ];
 
   # radarr
   services.radarr = {
     enable = true;
     dataDir = "/srv/state/radarr";
-    user = "radarr";
-    group = "media";
   };
-  users.users."radarr" = {
-    isSystemUser = true;
-    group = "media";
-    extraGroups = [ "torrenting" "usenet" ];
-  };
+  users.users."radarr".extraGroups = [ "media" "torrenting" "usenet" ];
 
   # sonarr 
   services.sonarr = {
     enable = true;
     dataDir = "/srv/state/sonarr";
-    user = "sonarr";
-    group = "media";
   };
-  users.users."sonarr" = {
-    isSystemUser = true;
-    group = "media";
-    extraGroups = [ "torrenting" "usenet" ];
+  users.users."sonarr".extraGroups = [ "media" "torrenting" "usenet" ];
+
+  ## transmission + wireguard
+
+  # wireguard
+  systemd.services."netns@" = {
+    before = [ "network.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.iproute}/bin/ip netns add %I";
+      ExecStop = "${pkgs.iproute}/bin/ip netns del %I";
+    };
   };
+  systemd.services.wg = {
+    bindsTo = [ "netns@wg.service" ];
+    requires = [ "network-online.target" ];
+    after = [ "netns@wg.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = with pkgs; writers.writeBash "wg-up" ''
+        set -e
+        ${iproute}/bin/ip link add wg0 type wireguard
+        ${iproute}/bin/ip link set wg0 netns wg
+        ${iproute}/bin/ip -n wg address add 68.235.44.2 dev wg0
+        ${iproute}/bin/ip netns exec wg \
+          ${wireguard}/bin/wg setconf wg0 /srv/state/wg0.conf
+        ${iproute}/bin/ip -n wg link set wg0 up
+        ${iproute}/bin/ip -n wg route add default dev wg0
+      '';
+      ExecStop = with pkgs; writers.writeBash "wg-down" ''
+        ${iproute}/bin/ip -n wg route del default dev wg0
+        ${iproute}/bin/ip -n wg link del wg0
+      '';
+    };
+  };
+
+  # transmission
+  services.transmission = {
+    enable = true;
+    webHome = pkgs.flood-for-transmission;
+    home = "/srv/state/transmission";
+    settings = {
+      download-dir = "/srv/storage/torrents";
+      incomplete-dir = "/srv/storage/torrents/incomplete";
+    };
+  }
+  users.users."transmission".extraGroups = [ "torrenting" ];
+  systemd.services.transmission = {
+    bindsTo = [ "netns@wg.service" ];
+    requires = [ "network-online.target" ];
+    after = [ "wg.service" ];
+    serviceConfig.NetworkNamespacePath = "/var/run/netns/wg";
+  }
+
+
 
   # media server
   #nixarr = {
