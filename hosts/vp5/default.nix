12f0f420f19e2d32491b899c79f55f934eb99cf0
n3mo <n3mo@no.one>
2024-08-02 13:21:28 -0400

    setting up wildcard certificate

--- hosts/vp5/default.nix
+++ hosts/vp5/default.nix
@@ -41,41 +41,40 @@
         };
       };
       # remote jellyfin access 
-      "n3mohomelab.xyz" = {
+      "jellyfin.n3mohomelab.xyz" = {
         forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://m3d14:8096";
-          extraConfig = ''
-            proxy_ssl_server_name on;
-            proxy_pass_header Authorization;
-          '';
-        };
+        useACMEHost = "n3mohomelab.xyz";
+        locations."/".proxyPass = "http://m3d14:8096";
       };
       # remote jellyseerr access 
       "jellyseerr.n3mohomelab.xyz" = {
         forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://m3d14:5055";
-          extraConfig = ''
-            proxy_ssl_server_name on;
-            proxy_pass_header Authorization;
-          '';
-        };
+        useACMEHost = "n3mohomelab.xyz";
+        locations."/".proxyPass = "http://m3d14:5055";
       };
     };
   };
 
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 443 ];
-  };
-
+  # acme
   security.acme = {
     acceptTerms = true;
     defaults.email = "jburrer@purdue.edu";
+    certs."n3mohomelab.xyz" = {
+      domain = "n3mohomelab.xyz";
+      extraDomainNames = [ "*.n3mohomelab.xyz" ];
+      dnsProvider = "vultr";
+      dnsPropagationCheck = true;
+      environmentFile = "${pkgs.writeText "vultr-creds" ''
+        VULTR_API_KEY=KYV2E5DMYYWELBZASVAVVKPW7JRVUJF3X6VQ
+      ''}";
+    };
+  };
+  users.users.nginx.extraGroups = [ "acme" ];
+
+  # open firewall for web hosting
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 ];
   };
 
   system.stateVersion = "23.05";
