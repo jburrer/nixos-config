24eaba72e0d7f7bed7510cd79b35a7a201343f38
n3mo <n3mo@no.one>
2024-08-29 22:07:42 -0400

    attempt 4 at fixing hangar6parking

--- hosts/vp5/hangar6parking.nix
+++ hosts/vp5/hangar6parking.nix
@@ -16,9 +16,6 @@
     locations."/" = {
       proxyPass = "http://127.0.0.1:8080";
       extraConfig = ''
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
-        proxy_set_header Host $http_host;
         proxy_redirect off;
         proxy_set_header Connection "";
         proxy_http_version 1.1;
