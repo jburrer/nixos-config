6018e5f9aad15feb0b42e031c7bd8e8fd4478fcb
n3mo <n3mo@no.one>
2025-03-18 14:24:58 -0400

    attempt 4 at adding transmission docker container

--- hosts/m3d14/homelab.nix
+++ hosts/m3d14/homelab.nix
@@ -164,7 +164,7 @@
     ];
     dependsOn = [ "tailscaleWithMullvad" ];
     extraOptions = [
-      "--network='service:tailscaleWithMullvad'"
+      "--network=service:tailscaleWithMullvad"
     ];
   };
   services.nginx.virtualHosts."transmission.local.n3mohomelab.xyz" = {
