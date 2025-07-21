9f2017ddf90784799dddae86ba2ec1b2081ed633
Johnny Burrer <jburrer@purdue.edu>
2025-07-21 12:07:23 -0400

    fixing traccar 3

--- hosts/m3d14/homelab.nix
+++ hosts/m3d14/homelab.nix
@@ -82,7 +82,7 @@
           };
         }
         {
-          "Other" = {
+          "Misc" = {
             style = "column";
             initiallyCollapsed = true;
           };
@@ -262,7 +262,7 @@
         ];
       }
       {
-        "Other" = [
+        "Misc" = [
           {
             "BK Management" = [
               {
@@ -551,22 +551,22 @@
   };
 
   # traccar
-  #virtualisation.oci-containers.containers."traccar" = {
-  #  image="traccar/traccar:latest";
-  #  ports = [
-  #    "8083:8082"
-  #    "5000-5300:5000-5300"
-  #    "5000-5300:5000-5300/udp"
-  #  ];
-  #  volumes = [
-  #    "/srv/state/traccar:/opt/traccar"
-  #  ];
-  #};
-  #services.nginx.virtualHosts."traccar.local.n3mohomelab.xyz" = {
-  #  forceSSL = true;
-  #  useACMEHost = "local.n3mohomelab.xyz";
-  #  locations."/".proxyPass = "http://localhost:8083";
-  #};
+  virtualisation.oci-containers.containers."traccar" = {
+    image="traccar/traccar:latest";
+    ports = [
+      "8083:8082"
+      "5000-5300:5000-5300"
+      "5000-5300:5000-5300/udp"
+    ];
+    volumes = [
+      "/srv/state/traccar:/opt/traccar"
+    ];
+  };
+  services.nginx.virtualHosts."traccar.local.n3mohomelab.xyz" = {
+    forceSSL = true;
+    useACMEHost = "local.n3mohomelab.xyz";
+    locations."/".proxyPass = "http://localhost:8083";
+  };
 
   # searxng 
   services.searx = {
