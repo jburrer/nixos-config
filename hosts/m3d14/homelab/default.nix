4fad54318b592812f11733efc266bcea108d410a
Johnny Burrer <jburrer@purdue.edu>
2025-10-19 20:01:55 -0400

    trying agnos 6

--- hosts/m3d14/homelab/default.nix
+++ hosts/m3d14/homelab/default.nix
@@ -39,13 +39,13 @@
 
   # agnos
   security = {
-    #acme = {
-    #  acceptTerms = true;
-    #  certs.".local.n3mohomelab.xyz" = {
-    #    email = "n3mo@startmail.com";
-    #    dnsProvider = "vultr";
-    #  };
-    #};
+    acme = {
+      acceptTerms = true;
+      certs.".local.n3mohomelab.xyz" = {
+        email = "n3mo@startmail.com";
+        dnsProvider = "vultr";
+      };
+    };
     agnos = {
       enable = true;
       #temporarilyOpenFirewall = true;
