17fd48726bf5e1eab29ad2692ea78c34978061a1
Johnny Burrer <jburrer@purdue.edu>
2025-12-21 20:59:44 -0500

    conduit server 5

--- hosts/m3d14/homelab/matrix.nix
+++ hosts/m3d14/homelab/matrix.nix
@@ -12,15 +12,15 @@
       "8448:6167"
     ];
     environment = [
-      "CONDUIT_SERVER_NAME=conduit.n3mohomelab.xyz"
-      "CONDUIT_DATABASE_BACKEND=rocksdb"
-      "CONDUIT_ALLOW_REGISTRATION=true"
-      "CONDUIT_ALLOW_FEDERATION=true"
-      "CONDUIT_MAX_REQUEST_SIZE=20000000"
-      "CONDUIT_TRUSTED_SERVERS=['matrix.org']"
-      "CONDUIT_MAX_CONCURRENT_REQUESTS=100"
-      "CONDUIT_PORT=6167"
-      "CONDUIT_CONFIG=''"
+      "CONDUIT_SERVER_NAME" = "conduit.n3mohomelab.xyz";
+      "CONDUIT_DATABASE_BACKEND" = "rocksdb";
+      "CONDUIT_ALLOW_REGISTRATION" = true;
+      "CONDUIT_ALLOW_FEDERATION" = true;
+      "CONDUIT_MAX_REQUEST_SIZE" = 20000000;
+      "CONDUIT_TRUSTED_SERVERS"= "[\"matrix.org\"]";
+      "CONDUIT_MAX_CONCURRENT_REQUESTS" = 100;
+      "CONDUIT_PORT" = 6167;
+      "CONDUIT_CONFIG" = "";
     ];
   };
   services.nginx.virtualHosts."conduit.n3mohomelab.xyz" = {
