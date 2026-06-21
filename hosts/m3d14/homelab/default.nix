109c5bd2d3026249d33376d44c3814070a17c55e
Johnny Burrer <jburrer@purdue.edu>
2026-06-20 23:32:03 -0400

    switching to modern fork of watchtower

--- hosts/m3d14/homelab/default.nix
+++ hosts/m3d14/homelab/default.nix
@@ -56,7 +56,7 @@
   # watchtower
   virtualisation.oci-containers.containers."watchtower" = {
     autoStart = true;
-    image = "containrrr/watchtower";
+    image = "nickfedor/watchtower";
     volumes = [
       "/var/run/docker.sock:/var/run/docker.sock"
     ]; 
